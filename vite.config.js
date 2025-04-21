0d53d3be21111b3b35e027ce21f128512bcd071f
DESKTOP-EV1O5OI\USER <raishasyauqi7@gmail.com>
2025-04-22 02:09:59 +0700

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-base: process.env.VITE_BASH_PATH || "/rdnaisha_sbd8",
+  plugins: [react()]
 });
